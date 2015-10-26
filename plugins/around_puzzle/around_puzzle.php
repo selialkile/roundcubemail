3936b7bfed206b74dc4c98d908484d6681a3ce2e
Thiago Coutinho <thiago.coutinho@locaweb.com.br>
2015-10-26 17:26:55 -0200

    nbla

--- plugins/around_puzzle/around_puzzle.php
+++ plugins/around_puzzle/around_puzzle.php
@@ -9,17 +9,20 @@
  * @license GNU GPLv3+
  * @author Thiago Coutinho
  */
+
 class around_puzzle extends rcube_plugin
 {
-  public $task = 'mail|around_puzzle';
+  public $task = 'around_puzzle';
 
   function init()
   {
+
     $this->rc = rcmail::get_instance();
     $this->register_task('around_puzzle');
     // register actions
     $this->register_action('around_puzzle', array($this, 'index'));
     $this->register_action('mailbox_list', array($this, 'mailbox_list'));
+    $this->register_action('preview', array($this, 'preview'));
 
     $this->add_hook('render_response', array($this, 'render_response'));
   }
@@ -27,7 +30,18 @@ class around_puzzle extends rcube_plugin
   function index()
   {
     //$this->rc->output->send('around_puzzle.index');
-    $this->to_render = array("foda" => "blabalblabal");
+    $this->to_render = array("testando" => "funciona!");
+  }
+
+  function preview(){
+    $RCMAIL = $this->rc;
+    $RCMAIL->storage_connect();
+    $mbox       = rcube_utils::get_input_value('mbox', rcube_utils::INPUT_GET);
+    $uid       = rcube_utils::get_input_value('_uid', rcube_utils::INPUT_GET);
+    $RCMAIL->storage->set_folder('INBOX');
+    $MESSAGE = new rcube_message($uid);
+
+    $this->to_render = $MESSAGE;
   }
 
   function mailbox_list()
@@ -35,30 +49,15 @@ class around_puzzle extends rcube_plugin
 
     $RCMAIL = $this->rc;
     $RCMAIL->storage_connect();
-
+    $RCMAIL->storage->set_folder('INBOX');
+    $RCMAIL->storage->set_page(1);
     $save_arr      = array();
     $dont_override = (array) $RCMAIL->config->get('dont_override');
 
-    // is there a sort type for this request?
-    if ($sort = rcube_utils::get_input_value('_sort', rcube_utils::INPUT_GET)) {
-        // yes, so set the sort vars
-        list($sort_col, $sort_order) = explode('_', $sort);
-
-        // set session vars for sort (so next page and task switch know how to sort)
-        if (!in_array('message_sort_col', $dont_override)) {
-            $_SESSION['sort_col'] = $save_arr['message_sort_col'] = $sort_col;
-        }
-        if (!in_array('message_sort_order', $dont_override)) {
-            $_SESSION['sort_order'] = $save_arr['message_sort_order'] = $sort_order;
-        }
-    }
-
     $mbox_name = $RCMAIL->storage->get_folder();
     $threading = (bool) $RCMAIL->storage->get_threading();
-
-    // Synchronize mailbox cache, handle flag changes
-    $RCMAIL->storage->folder_sync($mbox_name);
-
+    $RCMAIL->storage->set_folder($_SESSION['mbox'] = $mbox);
+    $RCMAIL->storage->set_page($_SESSION['page'] = $page);
     // fetch message headers
     if ($count = $RCMAIL->storage->count($mbox_name, $threading ? 'THREADS' : 'ALL', !empty($_REQUEST['_refresh']))) {
         $a_headers = $RCMAIL->storage->list_messages($mbox_name, NULL, 'DATE', 'DESC');
@@ -74,6 +73,28 @@ class around_puzzle extends rcube_plugin
     $render['threading'] = $threading;
     $render['current_page'] = $page;
     $render['exists'] = $exists;
+    $render['a_headers_count'] = count($a_headers);
+
+    $list = array();
+
+    foreach ($a_headers as &$header) {
+      $list[] = (object) array(
+        'uid' => $header->uid,
+        'subject' => rcube_mime::decode_header($header->subject, $header->charset),
+        'from' => rcube_mime::decode_header($header->from, $header->charset),
+        'to' => rcube_mime::decode_header($header->to, $header->charset),
+        // 'cc' => rcube_mime::decode_header($header->cc, $header->charset),
+        // 'replayto' => rcube_mime::decode_header($header->replayto, $header->charset),
+        // 'in_replay_to' => rcube_mime::decode_header($header->in_replay_to, $header->charset),
+        'date' => $header->date,
+        'size' => $header->size,
+        'internaldate' => $header->internaldate,
+        'priority' => $header->priority,
+        'folder' => $header->folder,
+        'flags' => $header->flags
+      );
+    }
+    $render['headers'] = $list;
 
 
     $this->to_render = $render;
@@ -81,7 +102,8 @@ class around_puzzle extends rcube_plugin
 
 
   function render_response($args){
-    $args['response'] =  $this->to_render;
-    return $args;
+    header('Content-Type: application/json; charset=' . $this->rc->output->get_charset());
+    echo @json_encode($this->to_render);
+    exit;
   }
 }
